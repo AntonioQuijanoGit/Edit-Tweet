5e5b2481a9d6c199d2c9a1d149e38afddd8c1adc
AntonioQuijanoGit <antonioquijanogit@gmail.com>
2024-09-16 10:33:04 +0200

    Add project

---
+++ src/helpers/blobToData.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+//https://stackoverflow.com/questions/25046301/convert-url-to-file-or-blob-for-filereader-readasdataurl
+
+export const blobToData = (blob) => {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+};
